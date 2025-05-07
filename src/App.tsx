236ae384d06973907429c12b91ba406905ac159f
Wellyson Martins <wellysonmartins@gmail.com>
2025-05-07 16:25:37 -0300

    add dark and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from 'sonner'
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet 
-        titleTemplate="%s | pizza.shop" 
-        defaultTitle="Pizza Shop" 
-      />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet 
+          titleTemplate="%s | pizza.shop" 
+          defaultTitle="Pizza Shop" 
+          />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
---
+++ src/components/account-menu.tsx
@@ -0,0 +1,32 @@
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+export default function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex items-center gap-2 select-none">
+          Pizza Shop
+
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Wellyson Martins</span>
+          <span className="text-xs font-normal text-muted-foreground">wellysonmartins@gmail.com</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
